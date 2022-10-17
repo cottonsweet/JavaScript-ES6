e88ca525009b6a3ba5c06bcfdfefa006476ac730
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-10-17 21:25:15 +0900

    shadow Dom, Template 으로 HTML 모듈화

--- Web-Components.js
+++ Web-Components.js
@@ -1,14 +1,26 @@
-class web extends HTMLElement {
+// class web extends HTMLElement {
+//   connectedCallback() {
+//     let name = this.getAttribute("name");
+//     this.innerHTML = `<div>${name}</div>`;
+//   }
+//   static get observedAttributes() {
+//     return ["name"];
+//   }
+//   attributeChangedCallback() {
+//     console.log(this.getAttribute["name"]);
+//   }
+// }
+
+// customElements.define("custom-input", web);
+
+class 클래스 extends HTMLElement {
   connectedCallback() {
-    let name = this.getAttribute("name");
-    this.innerHTML = `<div>${name}</div>`;
-  }
-  static get observedAttributes() {
-    return ["name"];
-  }
-  attributeChangedCallback() {
-    console.log(this.getAttribute["name"]);
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.append(template1.content.cloneNode(true));
+    let el = this.shadowRoot.querySelector("label");
+    el.addEventListener("click", function () {
+      console.log("클릭함");
+    });
   }
 }
-
-customElements.define("custom-input", web);
+customElements.define("custom-input", 클래스);
